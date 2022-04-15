1a9d1be7f46de5c5148f775d97ddcde39c4b4cea
TianyuQi <qitianyuqty@163.com>
2022-04-15 11:15:45 +0800

    0415am

--- FL_models/HierFL.py
+++ FL_models/HierFL.py
@@ -16,8 +16,24 @@ class Server(object):
         self.device, _, self.test_loader, self.model, _, self.optimizer, _, self.test_num = init_fuc(args)
         self.server_rref = rpc.RRef(self)
         self.edge_rrefs = []
-        self.world_size = args.world_size
         print("{} has received the {} data successfully!".format(rpc.get_worker_info().name, self.test_num))
+
+    def make_subnet(self, topo, args):
+        futs = []
+        for edge_rref in self.edge_rrefs:
+            futs.append(rpc.rpc_async(edge_rref.owner(), _call_method, args=(Edge.get_worker_rref, \
+                edge_rref, edge_rref.owner().id, topo, args), timeout=0))
+        for fut in futs:
+            fut.wait()
+        print('The subnet has been finished!')
+        
+    def run_edge_episode(self):
+        futs = []
+        for edge_rref in self.edge_rrefs:
+            futs.append(rpc.rpc_async(edge_rref.owner(), _call_method, args=(Edge.run_worker_episode, \
+                edge_rref), timeout=0))
+        for fut in futs:
+            fut.wait()
 
 
 class Edge(object):
@@ -25,6 +41,21 @@ class Edge(object):
         self.device, _, self.test_loader, self.model, _, self.optimizer, _, _ = init_fuc(args)
         self.edge_rref = rpc.RRef(self)
         self.worker_rrefs = []
+        
+    def get_worker_rref(self, id, topo, args):
+        for worker_rank in topo[id]:
+            worker_info = rpc.get_worker_info('worker{}'.format(worker_rank))
+            self.worker_rrefs.append(rpc.remote(worker_info, Worker, args=(args,)))
+        print("The Edge {} RRef map has been created successfully!".format(len(topo[id])))
+        print("The length of RRef is {}".format(len(self.worker_rrefs)))
+        
+    def run_worker_episode(self):
+        futs = []
+        for worker_rref in self.worker_rrefs:
+            futs.append(rpc.rpc_async(worker_rref.owner(), _call_method, args=(Worker.run_episode, \
+                worker_rref), timeout=0))
+        for fut in futs:
+            fut.wait()
 
 
 class Worker(object):
@@ -33,7 +64,7 @@ class Worker(object):
         self.idx = args.idx_user + 1
         print("{} has received the {} data successfully!".format(rpc.get_worker_info().name, self.train_num))
 
-    def run_episode(self, args):
+    def run_episode(self):
         import time
         from itertools import cycle
 
@@ -81,34 +112,34 @@ def run_worker(args):
     print("waiting for connecting......")
     topo = get_rank_list(args)
     print(topo)
-    # The 0 subnet
-    if args.subnet == 0:
-        # The server
-        if args.rank == 0:
-            os.environ['MASTER_ADDR'] = args.addr
-            os.environ['MASTER_PORT'] = args.port
-            os.environ["GLOO_SOCKET_IFNAME"] = "wlan0"
-            rpc.init_rpc(name='server', rank=args.rank, world_size=args.world_size)
-            print("{} has been initialized successfully".format(rpc.get_worker_info().name))
-            server = Server(args)
-            for edge_rank in range(1, args.world_size):
-                edge_info = rpc.get_worker_info('edge{}'.format(edge_rank))
-                server.edge_rrefs.append(rpc.remote(edge_info, Edge, args=(args,)))
-            print("The subnet {} RRef map has been created successfully!".format(args.subnet))
-            print("The length of RRef is {}".format(len(server.edge_rrefs)))
+    # The server
+    if args.rank == 0:
+        os.environ['MASTER_ADDR'] = args.addr
+        os.environ['MASTER_PORT'] = args.port
+        os.environ["GLOO_SOCKET_IFNAME"] = "wlan0"
+        rpc.init_rpc(name='server', rank=args.rank, world_size=args.world_size)
+        print("{} has been initialized successfully".format(rpc.get_worker_info().name))
+        server = Server(args)
+        for edge_rank in range(1, args.topo_num[0]):
+            edge_info = rpc.get_worker_info('edge{}'.format(edge_rank))
+            server.edge_rrefs.append(rpc.remote(edge_info, Edge, args=(args,)))
+        print("The Server {} RRef map has been created successfully!".format(args.topo_num[0]))
+        print("The length of RRef is {}".format(len(server.edge_rrefs)))
+        server.make_subnet(topo, args)
+        server.run_edge_episode()
 
-        else:
-            os.environ['MASTER_ADDR'] = args.addr
-            os.environ['MASTER_PORT'] = args.port
-            os.environ["GLOO_SOCKET_IFNAME"] = "eth0"
-            rpc.init_rpc(name='edge{}'.format(args.rank), rank=args.rank, world_size=args.world_size)
-            print("{} has been initialized successfully".format(rpc.get_worker_info().name))
+    if 0 < args.rank < args.topo_num[0]:
+        os.environ['MASTER_ADDR'] = args.addr
+        os.environ['MASTER_PORT'] = args.port
+        os.environ["GLOO_SOCKET_IFNAME"] = "eth0"
+        rpc.init_rpc(name='edge{}'.format(args.rank), rank=args.rank, world_size=args.world_size)
+        print("{} has been initialized successfully".format(rpc.get_worker_info().name))
     
     else:
         os.environ['MASTER_ADDR'] = args.addr
         os.environ['MASTER_PORT'] = args.port
         os.environ["GLOO_SOCKET_IFNAME"] = "wlan0"
-        rpc.init_rpc(name='worker{}'.format(args.rank), rank=args.rank, world_size=2)
+        rpc.init_rpc(name='worker{}'.format(args.rank), rank=args.rank, world_size=args.world_size)
         print("{} has been initialized successfully".format(rpc.get_worker_info().name))
 
     rpc.shutdown()
